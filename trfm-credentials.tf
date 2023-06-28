04c6c29dbb7e6b39f007909360883e0bc890de42
AnimelaAsif <110332127+AnimelaAsif@users.noreply.github.com>
2023-06-28 12:26:43 +0530

    Update trfm-credentials.tf

--- trfm-credentials.tf
+++ trfm-credentials.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "eu-west-2"
-  access_key = "AKIAQQCVSDFKJALGYZE2"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
